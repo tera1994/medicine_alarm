752821ff04562a046e5ea21dd9106c600695d745
tera <>
2023-09-29 11:29:24 +0900

    Delete debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Medication Confirmation Checklist'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
